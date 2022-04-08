848675b306bd18ec40614ca9bc15a0d3f0483594
MEHAMMEDAMINE <48122920+MEHAMMEDAMINE@users.noreply.github.com>
2022-04-08 10:56:41 +0100

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -13,7 +13,7 @@ logger = logging.getLogger("main")
 parser = argparse.ArgumentParser(description='train.py')
 ## Data options
 parser.add_argument('-traindata', default='dataset/long.json', help='Path to train data file')
-parser.add_argument('-testdata', default='dataset/semvalcomplet.json',help='Path to the test data file')
+parser.add_argument('-testdata', default='dataset/haadcomplet2.json',help='Path to the test data file')
 parser.add_argument('-valsplit', type=int, default=0,help='Number of examples for validation')
 parser.add_argument('-vocab_size', type=int, default=None, help='Limit vocabulary')
 parser.add_argument('-lowercase', action='store_true', default=False,help='Converting to lowercase')
@@ -63,7 +63,7 @@ parser.add_argument('-load_from', type=str, help='Path to a model checkpoint')
 ## Inference
 parser.add_argument('-eval', action='store_true',help='Evaluatation only mode')
 parser.add_argument('-interactive', action='store_true',help='Interactive mode')
-parser.add_argument('-max_train_decode_len', type=int, default=100,help='Max decoding length during training')
+parser.add_argument('-max_train_decode_len', type=int, default=151,help='Max decoding length during training')
 opt = parser.parse_args()
 
 def change_args(opt):
